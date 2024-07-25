1a2127e1345d8bac17a05738466997bb27d0af56
jesus1D <merlin.gustavo@escola.pr.gov.br>
2024-07-25 16:49:14 -0300

    .

--- pingpong.js
+++ pingpong.js
@@ -1,23 +1,22 @@
 //aqui eu vou criar a bolinha
 let xBolinha = 300;
-let yBolinha = 200; 
-let tamBolinha = 25; 
-function setup () {
+let yBolinha = 200;
+let tamBolinha = 25;
+
+function setup(){
     //aqui vou criar minha "mesa"
-    createCanvas (600,400);
+    createCanvas(600,400);
 }
 
 function draw(){
-    //aqui vou por a cor d "mesa"
-    background (75,0,130);
-    //chamado a função
-    criaBolinha (xBolinha, yBolinha, tamBolinha)
-
+    //aqui vou por a cor da "mesa"
+    //A cor da mesa é em rgb
+    background(128,128,0); 
+    //chamando a função
+    criaBolinha(xBolinha, yBolinha, tamBolinha);
 }
 
-
-
 //função bolinha
-function criarBolinha (Bolinha, yBolinha, tamBolinha)[
-    circle (xBolinha, yBolinha, tamBolinha);
-]
+function criaBolinha(xBolinha, yBolinha,tamBolinha){
+    circle (xBolinha,yBolinha,tamBolinha);
+}
