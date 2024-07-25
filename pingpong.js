b7e9b40192c1f470474f8e285ca7f1c674de2b3a
jesus1D <merlin.gustavo@escola.pr.gov.br>
2024-07-25 17:09:30 -0300

    .

--- pingpong.js
+++ pingpong.js
@@ -3,6 +3,10 @@ let xBolinha = 300;
 let yBolinha = 200;
 let tamBolinha = 25;
 
+//conig velocidade bolinha
+let xvelocidadeBolinha = 6;
+let yvelocidadeBolinha = 6;
+
 function setup(){
     //aqui vou criar minha "mesa"
     createCanvas(600,400);
@@ -19,4 +23,10 @@ function draw(){
 //função bolinha
 function criaBolinha(xBolinha, yBolinha,tamBolinha){
     circle (xBolinha,yBolinha,tamBolinha);
+}
+
+//função move bolinha 
+function moveBolinha(){
+    xBolinha = xvelocidadeBolinha + xBolinha;
+    yBolinha = yvelocidadeBolinha+ yBolinha;
 }
